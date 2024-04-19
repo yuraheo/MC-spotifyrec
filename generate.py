45221432ce3c9f00e16fbfe3f41b19b74d8da019
yuraheo <122466495+yuraheo@users.noreply.github.com>
2024-04-19 20:09:11 +0000

    learn.py test finish

--- generate.py
+++ generate.py
@@ -2,19 +2,23 @@ import sys
 import os.path
 import json
 import random
-# command call: python generate.py <length> <dictionary>
+# command call: python generate.py <length> <dictionary> <a_matrix>
 
 def main():
-    length, filename = readArguments()
-    dictionary = loadDictionary(filename)
+    length, dictfilename, matrixfilename = readArguments()
+    dictionary = loadDictionary(dictfilename)
+    a_matrix = loadMatrix(matrixfilename)
 
     lastSong = "~~~~~~~~~~~~~~~~" # sth we are unlikely to encounter at the begining
-    result = ""
+    result = "Music Recommendation: "
+    musicreclist = []
 
     for i in range(0, length):
-        newSong = getNextSong(lastSong, dictionary)
-        result = result + "->" + newSong
+        newSong = getNextSong(lastSong, dictionary, a_matrix)
+        musicreclist.append(newSong)
+        # result = result + "->" + newSong
         lastSong = newSong
+    result += " -> ".join(musicreclist)
 
     print(result)
 
@@ -22,14 +26,18 @@ def main():
 
 def readArguments():
     length = 50
-    filename = "dictionary.json"
+    dfilename = "dictionary.json"
+    mfilename = "adjacency.json"
 
     numArguments = len(sys.argv) - 1
     if numArguments >= 1:
         length = int(sys.argv[1])
     if numArguments >= 2:
-        filename = sys.argv[2]
-    return length, filename
+        dfilename = sys.argv[2]
+    if numArguments >= 3:
+        mfilename = sys.argv[3]
+
+    return length, dfilename, mfilename
 
 def loadDictionary(filename):
     if not os.path.exists(filename):
@@ -40,7 +48,16 @@ def loadDictionary(filename):
     file.close()
     return dictionary
 
-def getNextSong(lastSong, dict):
+def loadMatrix(filename):
+    if not os.path.exists(filename):
+        sys.exit("Error: Adjacency Matrix not found")
+        
+    file = open(filename, "r")
+    a_matrix = json.load(file)
+    file.close()
+    return a_matrix
+
+def getNextSong(lastSong, dict, a_matrix):
     if lastSong not in dict:
         # pick new random state
         newSong = pickRandom(dict)
@@ -48,12 +65,15 @@ def getNextSong(lastSong, dict):
 
     else:
         # pick new word from list
-        candidates = dict[lastSong]
+        lastIdx = dict[lastSong]
+        candidates = a_matrix[lastIdx] # list of frequencies
         candidatesNormalized = []
 
-        for song in candidates:
-            freq = candidates[song]
-            for i in range(0, freq):
+        for idx in range(1, len(candidates)):
+            freq = candidates[idx]
+            song = [k for k,v in dict.items() if v == idx][0]
+            
+            for _ in range(0, freq):
                 candidatesNormalized.append(song)
 
         rnd = random.randint(0, len(candidatesNormalized) - 1)
@@ -61,7 +81,7 @@ def getNextSong(lastSong, dict):
 
 def pickRandom(dict):
     randNum = random.randint(0, len(dict) - 1)
-    newSong = dict.keys()[randNum]
+    newSong = list(dict.keys())[randNum]
     return newSong
 
 main()
